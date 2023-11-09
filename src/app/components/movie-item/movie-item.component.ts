a4cae2b1a44006680d527e473fec804e791572fd
Alejandro <jimenezalejandro505@gmail.com>
2023-11-09 20:37:24 +0100

    Home casi terminada, error de visualizacion del body a solucinar, creo que por la falta de componetizacion entre el header y el nav

--- src/app/components/movie-item/movie-item.component.ts
+++ src/app/components/movie-item/movie-item.component.ts
@@ -8,8 +8,19 @@ import { Movie } from 'src/app/models/movie-list.interface';
 })
 export class MovieItemComponent {
   @Input() movie!: Movie;
+  longitudMaxima : number = 23;
 
   getPorcentaje(numero: number) {
     return numero * 10
+  }
+
+  acortarTitulo(titulo: string){
+    if (titulo.length > this.longitudMaxima) {
+      return titulo.substring(0, this.longitudMaxima
+        ) + '...';
+    } else {
+        return titulo;
+    }
+    
   }
 }

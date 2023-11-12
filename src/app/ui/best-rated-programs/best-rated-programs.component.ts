c4fef73700cee572c26c91e7b49aa109154c1839
Alejandro <jimenezalejandro505@gmail.com>
2023-11-12 21:50:37 +0100

    Best-rated programs terminado

--- src/app/ui/best-rated-programs/best-rated-programs.component.ts
+++ src/app/ui/best-rated-programs/best-rated-programs.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Program } from 'src/app/models/program.interface';
+import { ProgramService } from 'src/app/service/program.service';
 
 @Component({
   selector: 'app-best-rated-programs',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./best-rated-programs.component.css']
 })
 export class BestRatedProgramsComponent {
+
+  programList: Program[] = [];
+
+  count = 1000;
+  page = 1;
+
+  constructor(private programService: ProgramService) { }
+
+  ngOnInit(): void {
+    this.loadNewPage();
+  }
+
+  loadNewPage() {
+    this.programService.getRatedProgramList(this.page).subscribe(resp => {
+      this.programList = resp.results;
+    })
+  }
 
 }
